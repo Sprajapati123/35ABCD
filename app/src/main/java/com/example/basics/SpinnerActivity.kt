aaf5e82f5bf2448fc234283c4706ee94026e4523
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-05 13:36:41 +0545

    listview

--- app/src/main/java/com/example/basics/SpinnerActivity.kt
+++ app/src/main/java/com/example/basics/SpinnerActivity.kt
@@ -1,21 +1,26 @@
 package com.example.basics
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
 
 class SpinnerActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     lateinit var spinner : Spinner
     lateinit var textView: TextView
     lateinit var autoCompleteTextView: AutoCompleteTextView
+
+    lateinit var dateText : EditText
 
     val countries = arrayOf("Nepal",
         "India","China","Japan","Canada")
@@ -31,6 +36,14 @@ class SpinnerActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener 
         spinner = findViewById(R.id.spinner)
         textView = findViewById(R.id.displaySpinner)
         autoCompleteTextView = findViewById(R.id.autoCompleteTextView)
+        dateText = findViewById(R.id.editTextDate)
+
+
+        dateText.isFocusable = false
+        dateText.isClickable = true
+        dateText.setOnClickListener {
+            loadCalendar()
+        }
 
         var autoAdapter = ArrayAdapter(
             this@SpinnerActivity,
@@ -58,6 +71,25 @@ class SpinnerActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener 
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun loadCalendar() {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val days = c.get(Calendar.DAY_OF_MONTH)
+
+        val dialog = DatePickerDialog(
+            this@SpinnerActivity,
+            DatePickerDialog.OnDateSetListener
+            { datePicker, year, month, day ->
+                
+                dateText.setText("$year/${month+1}/$day")
+            },year,month,days
+        )
+        dialog.show()
+
+
     }
 
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
