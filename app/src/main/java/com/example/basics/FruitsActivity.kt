83f6d1b0dd1486260d26d6c9e648e0363633d319
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-10 11:37:14 +0545

    recycler

--- app/src/main/java/com/example/basics/FruitsActivity.kt
+++ app/src/main/java/com/example/basics/FruitsActivity.kt
@@ -7,12 +7,28 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class DashboardActivity : AppCompatActivity() {
-
+class FruitsActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+    val imageList = ArrayList<Int>()
+    val nameList = ArrayList<String>()
+    val descList = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_fruits)
+        recyclerView = findViewById(R.id.recyclerView)
+
+        imageList.add(R.drawable.apple)
+        imageList.add(R.drawable.mango)
+        imageList.add(R.drawable.grapes)
+
+        nameList.add("Apple")
+        nameList.add("Mango")
+        nameList.add("Grapes")
+
+        descList.add("This is apple")
+        descList.add("This is mango")
+        descList.add("This is grapes")
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
