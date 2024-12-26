d4d24a7d1bd974e826a9cf0b37117748def68edb
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-26 14:22:47 +0545

    nav bar

--- app/src/main/java/com/example/basics/ui/activity/NavigationActivity.kt
+++ app/src/main/java/com/example/basics/ui/activity/NavigationActivity.kt
@@ -9,34 +9,39 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.basics.R
-import com.example.basics.databinding.ActivityHomeBinding
-import com.example.basics.ui.fragment.FirstFragment
-import com.example.basics.ui.fragment.SecondFragment
+import com.example.basics.databinding.ActivityNavigationBinding
+import com.example.basics.ui.fragment.HomeFragment
+import com.example.basics.ui.fragment.NotificationFragment
+import com.example.basics.ui.fragment.ProfileFragment
+import com.example.basics.ui.fragment.SearchFragment
 
-class HomeActivity : AppCompatActivity() {
+class NavigationActivity : AppCompatActivity() {
+    lateinit var binding: ActivityNavigationBinding
 
-    lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        binding = ActivityNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        replaceFragment(FirstFragment())
+        replaceFragment(HomeFragment())
 
-        binding.btnFirst.setOnClickListener {
-            replaceFragment(FirstFragment())
+        binding.bottomView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navHome -> replaceFragment(HomeFragment())
+                R.id.navSearch -> replaceFragment(SearchFragment())
+                R.id.navNotification -> replaceFragment(NotificationFragment())
+                R.id.navProfile -> replaceFragment(ProfileFragment())
+                else -> {}
+            }
+            true
         }
-
-        binding.btnSecond.setOnClickListener {
-            replaceFragment(SecondFragment())
-        }
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
     }
@@ -47,7 +52,7 @@ class HomeActivity : AppCompatActivity() {
         val fragmentTransaction: FragmentTransaction =
             fragmentManager.beginTransaction()
         fragmentTransaction.replace(
-            binding.frameLayout.id,
+            binding.frameBottom.id,
             fragment
         )
         fragmentTransaction.commit()
